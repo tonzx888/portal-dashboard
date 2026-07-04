dc6febf5234ba533779534dbb6a4cd27b6fbbaf2
tonzx888 <anthony.smb8899@gmail.com>
2026-07-04 19:12:17 +0700

    Dashboard pertama

---
+++ script.js
@@ -0,0 +1,41 @@
+function lihatData() {
+
+    document.getElementById("data-staff").innerHTML = `
+
+        <div class="card">
+
+            <h3>Daftar Staff</h3>
+
+            <table>
+
+                <tr>
+                    <th>Nama</th>
+                    <th>Role</th>
+                    <th>Shift</th>
+                </tr>
+
+                <tr>
+                    <td>Andi</td>
+                    <td>CS</td>
+                    <td>Pagi</td>
+                </tr>
+
+                <tr>
+                    <td>Budi</td>
+                    <td>Kasir</td>
+                    <td>Malam</td>
+                </tr>
+
+                <tr>
+                    <td>Citra</td>
+                    <td>Kapten</td>
+                    <td>Pagi</td>
+                </tr>
+
+            </table>
+
+        </div>
+
+    `;
+
+}
